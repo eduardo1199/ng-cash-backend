98f6f4d941f9ee14fcd3237ce69757dc05fc1a58
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-03-16 16:21:56 -0300

    create routes transactions

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import fastify from 'fastify'
 import cookies from '@fastify/cookie'
 
 import { usersRoutes } from './routes/users'
+import { transactionsRoutes } from './routes/transactions'
 
 export const app = fastify({
   logger: true,
 })
 
 app.register(cookies)
+
 app.register(usersRoutes)
+app.register(transactionsRoutes)
